4cab043fce390403ccea4c8bb84e99e1f6e7775e
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-20 03:42:11 -0600

    Agregar middleware de autenticación y proteger rutas privadas

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,51 @@
+import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
 
+export const prerender = false;
+
+const privateRoutes = ['/protected'];
+
 // `context` and `next` are automatically typed
-export const onRequest = defineMiddleware(async(context, next) => {
+export const onRequest = defineMiddleware(async({url, request}, next) => {
 
-    return new Response(
-        JSON.stringify({
-            message: "Hello World",
-        }),
-    )
+    const authHeaders = request.headers.get("Authorization") ?? "";
+    console.log(authHeaders);
 
-    //return next();
+    if (privateRoutes.includes(url.pathname)) {
+
+        if (authHeaders) {
+            return checkAuth(authHeaders, next);
+        }
+
+    return new Response("Autenticación necesaria", {
+        status: 401,
+        headers: {
+            'WWW-Authenticate': 'Basic realm="Secure Area"'
+        }
+    });
+    }
+
+    return next();
 });
+
+const checkAuth = ( authHeaders: string, next: MiddlewareNext ) => {
+
+    if (authHeaders) {
+        const authValue = authHeaders.split(" ").at(-1) ?? 'user:pass';
+        const decodedValue = atob(authValue).split(":");
+        const [user, password] = decodedValue;
+
+        console.log(decodedValue)
+    
+        if (user === 'admin' && password === 'admin2') {
+            return next();
+        }
+    }
+
+    return new Response("Autenticación necesaria", {
+        status: 401,
+        headers: {
+            'WWW-Authenticate': 'Basic realm="Secure Area"'
+        },
+    });
+};
