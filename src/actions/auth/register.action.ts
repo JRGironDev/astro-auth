fabee71884742f2b4f54e868867c3bb8860cb90d
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-23 04:49:14 -0600

    Actualizar dependencias y mejorar el registro de usuario con manejo de errores y notificaciones

--- src/actions/auth/register.action.ts
+++ src/actions/auth/register.action.ts
@@ -1,5 +1,7 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { createUserWithEmailAndPassword, type AuthError } from "firebase/auth";
+import { firebase } from "@/firebase/config";
 
 export const registerUser = defineAction({
     accept: 'form',
@@ -10,6 +12,7 @@ export const registerUser = defineAction({
         remember_me: z.string().optional()
     }),
     handler: async ({ name, password, email, remember_me }, { cookies}) => {
+        // Cookies
         if ( remember_me ) 
         {
             cookies.set('email', email, {
@@ -20,9 +23,31 @@ export const registerUser = defineAction({
         {
             cookies.delete('email', {
                 path: '/'
-            })
+            });
         }
+        
+        // Create user
+        try {
+            const user = await createUserWithEmailAndPassword(
+                firebase.auth,
+                email,
+                password
+            );
 
-        return { ok: true, msj: "Registro exitoso" };
-    }
-})
+            // Actualizar usuario
+
+            // Verificar el correo electrónico
+            console.log(user);
+
+            return user;
+        } catch (error) {
+            const firebaseError = error as AuthError;
+            
+            if (firebaseError.code === "auth/email-already-in-use") {
+                throw new Error("El correo electrónico ya está en uso");
+            }
+            
+            throw new Error("Error al registrar el usuario");
+        }
+    },
+});
